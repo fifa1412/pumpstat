eebe792fc90d14c21c5483bad98fbfe313681015
fifa1412 <fifa1412@gmail.com>
2019-08-20 15:33:29 +0700

    Test 02

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Pump It Up : My Stat
+                    Pump It Up : My Stat 01
                 </div>
 
                 <div class="links">
