d3d187de8c0f1695f3cf7deed36d41153b1b373b
fifa1412 <fifa1412@gmail.com>
2019-08-20 15:31:14 +0700

    Test Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Pump It Up Stat
+                    Pump It Up : My Stat
                 </div>
 
                 <div class="links">
